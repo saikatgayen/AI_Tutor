443759f45ff84f0859e2ed199ab693045bc9c070
Saikat Gayen <gayensai5@gmail.com>
2026-01-28 21:05:08 +0530

    Added short-term memory management by the help of Sliding window memory concept

--- study_helper.py
+++ study_helper.py
@@ -4,14 +4,15 @@ from datetime import datetime
 print("AI TUITOR")
 print("Type 'exit' to quit.\n")
 
-conversation = []                                     #----Conversation Memory----
+conversation = []                                                  #----Conversation Memory----
+max_memory_messages = 6
 
-def save_to_file(questions, ansters):                              #--------Save Notes----- 
-	with open("notes.txt", "a", encoding="utf-8") as file:
-		file.write("\n" + "=" * 50 + "\n")
-		file.write(f"Time: {datetime.now()}\n")
-		file.write(f"Question: {questions}\n\n")
-		file.write(f"Answer:{answer}\n\n")
+def save_to_file(question, answer):                              #--------Save Notes----- 
+    with open("notes.txt", "a", encoding="utf-8") as file:
+        file.write("\n" + "=" * 50 + "\n")
+        file.write(f"Time: {datetime.now()}\n")
+        file.write(f"Question: {question}\n\n")
+        file.write(f"Answer:{answer}\n\n")
 
 while True:                                           #------Maiin Loop---------
     question = input("Ask a study question: ")
@@ -20,28 +21,31 @@ while True:                                           #------Maiin Loop---------
         print("Good luck with your studies! ")
         break
 
-        #ADD user question to memory
-
+						        #---------ADD user question to memory------
     conversation.append({
-		"role": "user",
-		"content": question
+        "role": "user",
+        "content": question
     })
 
 	#send FULL conversation to the model
 
     response = chat(
-        	model="llama3",
-        	messages= conversation
+        model="llama3",
+        messages= conversation
     )
 
     answer = response["message"]["content"]
 
-	# add AI answer to memory
+	                                                   #----------add AI answer to memory-----------
 
     conversation.append({
-		"role": "assistant",
-		"content": answer
+        "role": "assistant",
+        "content": answer
     })
+								
+    if len(conversation) > max_memory_messages:		#--------Memory Mangament---------------
+        conversation = conversation[-max_memory_messages:]
+
 
     print("\n Answer:\n")
     print(answer)
