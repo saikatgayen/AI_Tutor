46cb632394a3bc455576c512d9cdd9c8539b42c8
Saikat Gayen <gayensai5@gmail.com>
2026-01-28 21:24:22 +0530

    Update study_helper.py

--- study_helper.py
+++ study_helper.py
@@ -7,12 +7,21 @@ print("Type 'exit' to quit.\n")
 conversation = []                                                  #----Conversation Memory----
 max_memory_messages = 6
 
+<<<<<<< Updated upstream
 def save_to_file(question, answer):                              #--------Save Notes----- 
     with open("notes.txt", "a", encoding="utf-8") as file:
         file.write("\n" + "=" * 50 + "\n")
         file.write(f"Time: {datetime.now()}\n")
         file.write(f"Question: {question}\n\n")
         file.write(f"Answer:{answer}\n\n")
+=======
+def save_to_file(questions, ansters):                              #--------Save Notes----- 
+  with open("notes.txt", "a", encoding="utf-8") as file:
+    file.write("\n" + "=" * 50 + "\n")
+    file.write(f"Time: {datetime.now()}\n")
+    file.write(f"Question: {questions}\n\n")
+    file.write(f"Answer:{answer}\n\n")
+>>>>>>> Stashed changes
 
 while True:                                           #------Maiin Loop---------
     question = input("Ask a study question: ")
@@ -23,24 +32,42 @@ while True:                                           #------Maiin Loop---------
 
 						        #---------ADD user question to memory------
     conversation.append({
+<<<<<<< Updated upstream
         "role": "user",
         "content": question
+=======
+    "role": "user",
+    "content": question
+>>>>>>> Stashed changes
     })
 
-	#send FULL conversation to the model
+  #send FULL conversation to the model
 
     response = chat(
+<<<<<<< Updated upstream
         model="llama3",
         messages= conversation
+=======
+          model="llama3",
+          messages= conversation
+>>>>>>> Stashed changes
     )
 
     answer = response["message"]["content"]
 
+<<<<<<< Updated upstream
 	                                                   #----------add AI answer to memory-----------
 
     conversation.append({
         "role": "assistant",
         "content": answer
+=======
+  # add AI answer to memory
+
+    conversation.append({
+    "role": "assistant",
+    "content": answer
+>>>>>>> Stashed changes
     })
 								
     if len(conversation) > max_memory_messages:		#--------Memory Mangament---------------
